7f2bc9ac3cd086784fb89f29a996e2a3d55b34f8
mattjames <matt.james@patrontechnology.com>
2021-07-14 20:54:50 +0800

    initial commit.

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ data "template_file" "userdata_snippet" {
   template = file("${path.module}/bash.tpl")
   vars = {
     ec2_fqdn_tag        = var.ec2_fqdn_tag
-    aws_secret_id       = var.ssh_ca_signing_cert_secret
+    aws_secret_id       = var.ssh_ca_signing_cert_secret_arn
   }
 }
