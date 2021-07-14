752cbc82ed806951e467da5975029321e46ffb3b
mattjames <matt.james@patrontechnology.com>
2021-07-14 22:16:11 +0800

    initial commit.

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "freeipa_userdata_snippet" {
+output "userdata_snippet" {
   description = "Userdata snippet to be added to EC2 instance during bootstrap"
   value       = data.template_file.userdata_snippet.rendered
 }
